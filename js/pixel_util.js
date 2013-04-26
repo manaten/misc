20d86d89872049a1b56d32334a3bd9653392706a
manaten <manaten@manaten.net>
2013-04-26 09:21:02 -0700

    とりあえずpixel_utilがちゃんと動作するようにした

--- js/pixel_util.js
+++ js/pixel_util.js
@@ -7,99 +7,74 @@ var PixelUtil = (function() {
 	 * constructor
 	 * @param String url   The url of image
 	 */
-	var PixelUtil = function(url) {
+	var PixelImage = function(url, byteData) {
 		this.url = url;
-		this.byteData = [];
+		this.byteData = byteData;
 		this.palette = null;
 		this.colorDepth = 0;
 	};
-
-	/**
-	 * convert text to byte array.
-	 */
-	var textToByteArray = function(text) {
-		var byteArray = new Array(text.length);
-		for (var i = 0; i < text.length; i++) {
-			byteArray[i] = text.charCodeAt(i) & 0xff;
-		}
-		return byteArray;
-	};
-	/**
-	 * load image bytes.
-	 * @param function callback   The function called after load.
-	 */
-	PixelUtil.prototype.load = function(callback) {
-		var that = this;
-		var request = new XMLHttpRequest();
-		request.open('GET', this.url, true);
-		request.overrideMimeType('text\/plain; charset=x-user-defined');
-		request.onload = function() {
-			if (request.status != 200) {
-				throw new Exception("Can't load image: " + this.url);
-			}
-			that.byteData = request.responseText;
-			callback && callback();
-		};
-		request.send(null);
-	};
-
-	PixelUtil.prototype.getFileSize = function() {
+	
+	PixelImage.prototype.getFileSize = function() {
 		return this.byteData.length;
 	};
 
-	PixelUtil.prototype.isPng = function() {
+	PixelImage.prototype.isPng = function() {
 		var src = this.byteData;
-		return src.length > 24
+		return (src.length > 24)
 			&& src[0]===0x89 && src[1]===0x50 && src[2]===0x4E && src[3]===0x47
 			&& src[4]===0x0D && src[5]===0x0A && src[6]===0x1A && src[7]===0x0A;
 	};
 
-	PixelUtil.prototype.isGif = function() {
+	PixelImage.prototype.isGif = function() {
 		var src = this.byteData;
 		return src.length > 22 && src[0]===0x47 && src[1]===0x49 && src[2]===0x46;
 	};
 
-	PixelUtil.prototype.getWidth = function() {
+	PixelImage.prototype.getWidth = function() {
 		var src = this.byteData;
 		var width = 0;
-		if (isPNG()) {
+		if (this.isPng()) {
 			width += src[16] << 24;
 			width += src[17] << 16;
 			width += src[18] << 8;
 			width += src[19];
-		} else if (isGIF()) {
+		} else if (this.isGif()) {
 			width += src[7] << 8;
 			width += src[6];
 		}
 		return width;
 	};
 
-	PixelUtil.prototype.getHeight = function() {
+	PixelImage.prototype.getHeight = function() {
 		var src = this.byteData;
 		var height = 0;
-		if (isPNG()) {
+		if (this.isPng()) {
 			height += src[20] << 24;
 			height += src[21] << 16;
 			height += src[22] << 8;
 			height += src[23];
-		} else if (isGIF()) {
+		} else if (this.isGif()) {
 			height += src[9] << 8;
 			height += src[8];
 		}
 		return height;
 	};
 
+	var toHexString = function(num) {
+		return (num < 10 ? "0" : "") + num.toString(16);
+	};
+
 	var getPaletteFromSource = function(src, colorDepth, offset) {
 		var size = colorDepth * colorDepth;
 		if (offset + size * 3 > src.length) {
-			throw new Exception("Illigal image file.: " + this.url);
+			throw "Illigal image file.: " + this.url;
 		}
 		var palette = new Array(size);
 		for(var i = 0; i < size; i++) {
 			var red   = src[i*3+offset];
 			var green = src[i*3+offset+1];
 			var blue  = src[i*3+offset+2];
-			palette[i] = (red<<16).toString(16) + (green<<8).toString(16) + blue.toString(16);
+			palette[i] = toHexString(red) + toHexString(green) + toHexString(blue);
 		}
 		return palette;
 	};
@@ -121,7 +96,7 @@ var PixelUtil = (function() {
 				offset += 12 + (src[offset]<<24) + (src[offset+1]<<16) + (src[offset+2]<<8) + (src[offset+3]);
 			}
 		} else {
-			throw new Exception("Png file is not a indexed image.: " + this.url);
+			throw "Png file is not a indexed image.: " + this.url;
 		}
 		this.colorDepth = colorDepth;
 		return getPaletteFromSource(src, colorDepth, offset);
@@ -167,10 +142,10 @@ var PixelUtil = (function() {
 						}
 						offset += 1;
 					} else {
-						throw new Exception("Unsupported GIF file.: " + this.url);
+						throw "Unsupported GIF file.: " + this.url;
 					}
 				} else {
-					throw new Exception("Unsupported GIF file.: " + this.url);
+					throw "Unsupported GIF file.: " + this.url;
 				}
 			}
 		}
@@ -178,17 +153,47 @@ var PixelUtil = (function() {
 		return getPaletteFromSource(src, colorDepth, offset);
 	};
 
-	PixelUtil.prototype.getPalette = function() {
+	PixelImage.prototype.getPalette = function() {
 		// momerize
 		if (this.palette !== null) {
 			return this.palette;
 		}
-		if (isPNG()) {
+		if (this.isPng()) {
 			return getPngPalette.apply(this, []);
-		} else if (isGIF()) {
+		} else if (this.isGif()) {
 			return getGifPalette.apply(this, []);
 		}
-		throw new Exception("File is not PNG or GIF.: " + this.url);
+		throw "File is not PNG or GIF.: " + this.url;
+	};
+
+
+	var PixelUtil = {};
+	/**
+	 * convert text to byte array.
+	 */
+	var textToByteArray = function(text) {
+		var byteArray = new Array(text.length);
+		for (var i = 0; i < text.length; i++) {
+			byteArray[i] = text.charCodeAt(i) & 0xff;
+		}
+		return byteArray;
+	};
+	/**
+	 * load image bytes.
+	 * @param function callback   The function called after load.
+	 */
+	PixelUtil.load = function(url, callback) {
+		var request = new XMLHttpRequest();
+		request.open('GET', url, true);
+		request.overrideMimeType('text\/plain; charset=x-user-defined');
+		request.onload = function() {
+			if (request.status != 200) {
+				throw "Can't load image: " + url;
+			}
+			var byteData = textToByteArray(request.responseText);
+			callback && callback(new PixelImage(url, byteData));
+		};
+		request.send(null);
 	};
 	return PixelUtil;
 })();
