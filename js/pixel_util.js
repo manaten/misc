386d27667e43d9ea5cb1339b360e128775357d39
manaten <manaten@manaten.net>
2013-04-28 09:08:46 -0700

    Tip表示関数を追加

--- js/pixel_util.js
+++ js/pixel_util.js
@@ -1,198 +1,283 @@
 "use strict";
 /**
- * The class handle image pixels
+ * A util class of pixel art.
  */
 var PixelUtil = (function() {
 	/**
-	 * constructor
+	 * The class handle image pixels
 	 */
-	var PixelImage = function(url, byteData) {
-		this.url = url;
-		this.byteData = byteData;
-		this.palette = null;
-		this.colorDepth = 0;
-	};
-	
-	/**
-	 * Return the byte size of image.
-	 */
-	PixelImage.prototype.getFileSize = function() {
-		return this.byteData.length;
-	};
+	var PixelImage = function() {
+		var PixelImage = function(url, byteData) {
+			this.url = url;
+			this.name = url.split('/').pop();
+			this.byteData = byteData;
+			this.palette = null;
+			this.colorDepth = 0;
+		};
+		
+		/**
+		 * Return the byte size of image.
+		 */
+		PixelImage.prototype.getFileSize = function() {
+			return this.byteData.length;
+		};
 
-	/**
-	 * Return true when image is png otherwise false.
-	 */
-	PixelImage.prototype.isPng = function() {
-		var src = this.byteData;
-		return (src.length > 24)
-			&& src[0]===0x89 && src[1]===0x50 && src[2]===0x4E && src[3]===0x47
-			&& src[4]===0x0D && src[5]===0x0A && src[6]===0x1A && src[7]===0x0A;
-	};
+		/**
+		 * Return true when image is png otherwise false.
+		 */
+		PixelImage.prototype.isPng = function() {
+			var src = this.byteData;
+			return (src.length > 24)
+				&& src[0]===0x89 && src[1]===0x50 && src[2]===0x4E && src[3]===0x47
+				&& src[4]===0x0D && src[5]===0x0A && src[6]===0x1A && src[7]===0x0A;
+		};
 
-	/**
-	 * Return true when image is git otherwise false.
-	 */
-	PixelImage.prototype.isGif = function() {
-		var src = this.byteData;
-		return src.length > 22 && src[0]===0x47 && src[1]===0x49 && src[2]===0x46;
-	};
+		/**
+		 * Return true when image is git otherwise false.
+		 */
+		PixelImage.prototype.isGif = function() {
+			var src = this.byteData;
+			return src.length > 22 && src[0]===0x47 && src[1]===0x49 && src[2]===0x46;
+		};
 
-	/**
-	 * Get the width of image.
-	 */
-	PixelImage.prototype.getWidth = function() {
-		var src = this.byteData;
-		var width = 0;
-		if (this.isPng()) {
-			width += src[16] << 24;
-			width += src[17] << 16;
-			width += src[18] << 8;
-			width += src[19];
-		} else if (this.isGif()) {
-			width += src[7] << 8;
-			width += src[6];
-		}
-		return width;
-	};
-
-	/**
-	 * Get the height of image.
-	 */
-	PixelImage.prototype.getHeight = function() {
-		var src = this.byteData;
-		var height = 0;
-		if (this.isPng()) {
-			height += src[20] << 24;
-			height += src[21] << 16;
-			height += src[22] << 8;
-			height += src[23];
-		} else if (this.isGif()) {
-			height += src[9] << 8;
-			height += src[8];
-		}
-		return height;
-	};
-
-	var toHexString = function(num) {
-		return (num < 0x10 ? "0" : "") + num.toString(16);
-	};
-
-	var getPaletteFromSource = function(src, colorDepth, offset) {
-		var size = Math.pow(2, colorDepth);
-		if (offset + size * 3 > src.length) {
-			throw "Illigal image file.: " + this.url;
-		}
-		var palette = new Array(size);
-		for(var i = 0; i < size; i++) {
-			var red   = src[i*3+offset];
-			var green = src[i*3+offset+1];
-			var blue  = src[i*3+offset+2];
-			palette[i] = toHexString(red) + toHexString(green) + toHexString(blue);
-		}
-		return palette;
-	};
-
-	var getPngPalette = function() {
-		var src = this.byteData;
-		var colorDepth = src[24];
-		var offset = 8;
-		if (src[25] === 3) {
-			// seek a chank of 'PLTE'
-			while (offset < src.length) {
-				if ((src[offset+4] === 0x50)    // P
-				 && (src[offset+5] === 0x4C)    // L
-				 && (src[offset+6] === 0x54)    // T
-				 && (src[offset+7] === 0x45)) { // E
-					offset += 8;
-					break;
-				}
-				offset += 12 + (src[offset]<<24) + (src[offset+1]<<16) + (src[offset+2]<<8) + (src[offset+3]);
+		/**
+		 * Get the width of image.
+		 */
+		PixelImage.prototype.getWidth = function() {
+			var src = this.byteData;
+			var width = 0;
+			if (this.isPng()) {
+				width += src[16] << 24;
+				width += src[17] << 16;
+				width += src[18] << 8;
+				width += src[19];
+			} else if (this.isGif()) {
+				width += src[7] << 8;
+				width += src[6];
 			}
-		} else {
-			throw "Png file is not a indexed image.: " + this.url;
-		}
-		this.colorDepth = colorDepth;
-		return getPaletteFromSource(src, colorDepth, offset);
-	};
+			return width;
+		};
 
-	var getGifPalette = function() {
-		var src = this.byteData;
-		var colorDepth = 0;
-		var offset = 13;
-		var gctf = (src[10]>>7) & 0x1;
-		if (gctf === 1) {
-			colorDepth = (src[10] & 0x7) + 1;
-		} else {
-			// if gif file not has gct, seeking first lct and get offset to there.
-			while (offset < src.length) {
-				if (src[offset] === 0x2c) {
-					colorDepth = (src[offset + 9] & 0x7) + 1;
-					offset += 10;
-					break;
-				} else if (src[offset] === 0x21) {
-					if (src[offset + 1] === 0xf9) {
-						// Graphic Control Extension
+		/**
+		 * Get the height of image.
+		 */
+		PixelImage.prototype.getHeight = function() {
+			var src = this.byteData;
+			var height = 0;
+			if (this.isPng()) {
+				height += src[20] << 24;
+				height += src[21] << 16;
+				height += src[22] << 8;
+				height += src[23];
+			} else if (this.isGif()) {
+				height += src[9] << 8;
+				height += src[8];
+			}
+			return height;
+		};
+
+		var toHexString = function(num) {
+			return (num < 0x10 ? "0" : "") + num.toString(16);
+		};
+
+		var getPaletteFromSource = function(src, colorDepth, offset) {
+			var size = Math.pow(2, colorDepth);
+			if (offset + size * 3 > src.length) {
+				throw "Illigal image file.: " + this.url;
+			}
+			var palette = new Array(size);
+			for(var i = 0; i < size; i++) {
+				var red   = src[i*3+offset];
+				var green = src[i*3+offset+1];
+				var blue  = src[i*3+offset+2];
+				palette[i] = toHexString(red) + toHexString(green) + toHexString(blue);
+			}
+			return palette;
+		};
+
+		var getPngPalette = function() {
+			var src = this.byteData;
+			var colorDepth = src[24];
+			var offset = 8;
+			if (src[25] === 3) {
+				// seek a chank of 'PLTE'
+				while (offset < src.length) {
+					if ((src[offset+4] === 0x50)    // P
+					 && (src[offset+5] === 0x4C)    // L
+					 && (src[offset+6] === 0x54)    // T
+					 && (src[offset+7] === 0x45)) { // E
 						offset += 8;
-					} else if (src[offset + 1] === 0xfe) {
-						// Comment Extension
-						offset += 2;
-						while (src[offset] !== 0) {
-							offset += src[offset] + 1;
+						break;
+					}
+					offset += 12 + (src[offset]<<24) + (src[offset+1]<<16) + (src[offset+2]<<8) + (src[offset+3]);
+				}
+			} else {
+				throw "Png file is not a indexed image.: " + this.url;
+			}
+			this.colorDepth = colorDepth;
+			return getPaletteFromSource(src, colorDepth, offset);
+		};
+
+		var getGifPalette = function() {
+			var src = this.byteData;
+			var colorDepth = 0;
+			var offset = 13;
+			var gctf = (src[10]>>7) & 0x1;
+			if (gctf === 1) {
+				colorDepth = (src[10] & 0x7) + 1;
+			} else {
+				// if gif file not has gct, seeking first lct and get offset to there.
+				while (offset < src.length) {
+					if (src[offset] === 0x2c) {
+						colorDepth = (src[offset + 9] & 0x7) + 1;
+						offset += 10;
+						break;
+					} else if (src[offset] === 0x21) {
+						if (src[offset + 1] === 0xf9) {
+							// Graphic Control Extension
+							offset += 8;
+						} else if (src[offset + 1] === 0xfe) {
+							// Comment Extension
+							offset += 2;
+							while (src[offset] !== 0) {
+								offset += src[offset] + 1;
+							}
+							offset += 1;
+						} else if (src[offset + 1] === 0x01) {
+							// Plain Text Extension
+							offset += 15;
+							while (src[offset] != 0) {
+								offset += src[offset] + 1;
+							}
+							offset += 1;
+						} else if (src[offset + 1] === 0xff) {
+							// Application Extension
+							offset += 14;
+							while (src[offset] != 0) {
+								offset += src[offset] + 1;
+							}
+							offset += 1;
+						} else {
+							throw "Unsupported GIF file.: " + this.url;
 						}
-						offset += 1;
-					} else if (src[offset + 1] === 0x01) {
-						// Plain Text Extension
-						offset += 15;
-						while (src[offset] != 0) {
-							offset += src[offset] + 1;
-						}
-						offset += 1;
-					} else if (src[offset + 1] === 0xff) {
-						// Application Extension
-						offset += 14;
-						while (src[offset] != 0) {
-							offset += src[offset] + 1;
-						}
-						offset += 1;
 					} else {
 						throw "Unsupported GIF file.: " + this.url;
 					}
-				} else {
-					throw "Unsupported GIF file.: " + this.url;
 				}
 			}
-		}
-		this.colorDepth = colorDepth;
-		return getPaletteFromSource(src, colorDepth, offset);
-	};
+			this.colorDepth = colorDepth;
+			return getPaletteFromSource(src, colorDepth, offset);
+		};
+
+		/**
+		 * Get the palette array of image.
+		 */
+		PixelImage.prototype.getPalette = function() {
+			// momerize
+			if (this.palette !== null) {
+				return this.palette;
+			}
+			if (this.isPng()) {
+				return getPngPalette.apply(this, []);
+			} else if (this.isGif()) {
+				return getGifPalette.apply(this, []);
+			}
+			throw "File is not PNG or GIF.: " + this.url;
+		};
+		return PixelImage;
+	}();
+
 
 	/**
-	 * Get the palette array of image.
+	 * A tip class
 	 */
-	PixelImage.prototype.getPalette = function() {
-		// momerize
-		if (this.palette !== null) {
-			return this.palette;
-		}
-		if (this.isPng()) {
-			return getPngPalette.apply(this, []);
-		} else if (this.isGif()) {
-			return getGifPalette.apply(this, []);
-		}
-		throw "File is not PNG or GIF.: " + this.url;
-	};
+	var PixelTip =  function() {
+		var PixelTip = function($img, imgInfo) {
+			var that = this;
+			this.$img = $("<img src='" + $img.attr('src') + "'>")
+				.css({ "background-color":bgcolors[0] });
+			this.bgcolor = 0;
+			this.zoomLevel = 1;
+			this.baseWidth  = $img.attr("width");
+			this.baseHeight = $img.attr("height");
+
+			var $controll = $("<div class='controll'>" + imgInfo.name + "</div>");
+			$('<button class="zoomIn" type="button">zoomIn</button>')
+				.appendTo($controll)
+				.click( function() { zoomIn.apply(that) } );
+			$('<button class="zoomOut" type="button">zoomOut</button>')
+				.appendTo($controll)
+				.click( function() { zoomOut.apply(that) } );
+			$('<button class="bgColor" type="button">bgColor</button>')
+				.appendTo($controll)
+				.click( function() { changeBGColor.apply(that) } );
+
+			var $colorDiv = $('<div class="color">hover to palette</div>');
+			var pos = $img.offset();
+			var $tip = $("<div class='pixelTip'></div>")
+				.append($controll)
+				.append($('<div class="container"></div>').append(this.$img))
+				.append("<div>" +
+					"<span class='width'>"  + imgInfo.width      + "</span>" +
+					"<span class='height'>" + imgInfo.height     + "</span>" +
+					"<span class='size'>"   + imgInfo.fileSize   + "</span>" +
+					"<span class='depth'>"  + imgInfo.colorDepth + "</span></div>")
+				.append( createPaletteTable(imgInfo.palette, $colorDiv) )
+				.append($colorDiv)
+				.css({ position:"absolute", "left":pos.left-10+"px", "top":pos.top-10+"px" })
+				.hide()
+				.hover(null, function(ev) { $(ev.target).is('.pixelTip') && $tip.hide(); } )
+				.appendTo(document.body);
+			$img.mouseover(function() { $tip.show(); } );
+
+		};
+
+		var createPaletteTable = function(palette, $colorDiv) {
+			var $table = $("<table class='palette'></table>");
+			for (var y = 0; y < 16; y++) {
+				var $tr = $("<tr></tr>");
+				for (var x = 0; x < 16; x++) {
+					var offset = x + y * 16;
+					(function() {
+						var color = (offset >= palette.length) ? "#000000" : "#" + palette[offset];
+						$("<td></td>")
+							.css({ "background-color":color })
+							.mouseover(function() { $colorDiv.text(color); })
+							.appendTo($tr);
+					})();
+				}
+				$table.append($tr);
+			}
+			return $table;
+		};
+
+		var bgcolors = ["#FFF", "#FCC", "#CFC", "#CCF", "#F33", "#3F3", "#33F", "#FF3", "#3FF", "#F3F", "#000"];
+		var changeBGColor = function() {
+			this.bgcolor = this.bgcolor >= bgcolors.length-1 ? 0 : this.bgcolor+1;
+			this.$img.css("background-color", bgcolors[this.bgcolor]);
+		};
+
+		var zoomIn = function() {
+			this.zoomLevel = this.zoomLevel < 8 ? this.zoomLevel*2 : this.zoomLevel;
+			fixSize.apply(this);
+		};
+
+		var zoomOut = function() {
+			this.zoomLevel = this.zoomLevel > 1 ? this.zoomLevel/2 : this.zoomLevel;
+			fixSize.apply(this);
+		};
+
+		var fixSize = function() {
+			this.$img.css({ width: this.baseWidth*this.zoomLevel+"px", height: this.baseHeight*this.zoomLevel+"px" });
+		};
+		return PixelTip;
+	}();
 
 	var PixelUtil = {};
 	var getXmlHttpObject = function() {
-		var xhr;
-		if (XMLHttpRequest) {
-			xhr = new XMLHttpRequest();
-		} else {
-			xhr = new ActiveXObject('MSXML2.XMLHTTP.6.0');
-		}
-		return xhr;
+		return XMLHttpRequest ?  new XMLHttpRequest() : new ActiveXObject('MSXML2.XMLHTTP.6.0');
 	};
+
 	/**
 	 * convert text to byte array.
 	 */
@@ -203,6 +288,7 @@ var PixelUtil = (function() {
 		}
 		return byteArray;
 	};
+
 	/**
 	 * load image and get informations of image.
 	 * @param String url The url of image.
@@ -213,16 +299,17 @@ var PixelUtil = (function() {
 	PixelUtil.load = function(url, callback) {
 		var request = getXmlHttpObject();
 		request.open('GET', url, true);
-		request.overrideMimeType('text\/plain; charset=x-user-defined');
+		request.overrideMimeType && request.overrideMimeType('text\/plain; charset=x-user-defined');
 		request.onload = function() {
 			if (request.status != 200) {
 				throw "Couldn't load image: " + url;
 			}
 			var byteData = textToByteArray(request.responseText);
 			var img = new PixelImage(url, byteData);
-			// call callback with informations of image.
+			// callback with informations of image.
 			callback({
 				url: url,
+				name: img.name,
 				fileSize:   img.getFileSize(),
 				width:      img.getWidth(),
 				height:     img.getHeight(),
@@ -234,5 +321,18 @@ var PixelUtil = (function() {
 		};
 		request.send(null);
 	};
+
+	/**
+	 * Create a tip of image.
+	 * When mouce hover to image, show a tip of image.
+	 * this method can't work without jQuery
+	 */
+	PixelUtil.createTip = function(img) {
+		var $img = $(img);
+		PixelUtil.load($img.attr('src'), function(imgInfo) {
+			new PixelTip($img, imgInfo);
+		});
+	};
+
 	return PixelUtil;
 })();
