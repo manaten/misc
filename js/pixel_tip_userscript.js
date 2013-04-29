026c060c3b46336d59964a0061974c9104834d50
manaten <manaten@manaten.net>
2013-04-29 05:38:20 -0700

    userscript用のjsを追加

---
+++ js/pixel_tip_userscript.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name        PixelTip
+// @namespace   http://manaten.net
+// @description 特定のページで画像にドット絵向けTipを表示します。
+// @include     http://www.pixeljoint.com/*
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require     https://raw.github.com/manaten/misc/master/js/pixel_util.js
+// @resource    PixelTip https://raw.github.com/manaten/misc/master/css/pixel_tip.css
+// @version     1
+// @grant       GM_addStyle
+// @grant       GM_getResourceText
+// ==/UserScript==
+
+$(function() {
+  GM_addStyle(GM_getResourceText("PixelTip"));
+  $('img').each(function(_, img) {
+    var $img = $(img);
+    var tip = PixelUtil.createTip($img.attr('src'));
+    $img.hover(function() {
+      var pos = $img.offset();
+      tip.show(pos.left-10, pos.top-10);
+    }, null);
+  });
+});
